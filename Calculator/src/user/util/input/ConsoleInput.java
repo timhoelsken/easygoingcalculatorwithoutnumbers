fc40b36f15042a3c87465bf87ea67e54a32f2437
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-02-29 12:49:10 +0000

    + Class for reading User input from the console

---
+++ Calculator/src/user/util/input/ConsoleInput.java
@@ -0,0 +1,28 @@
+package user.util.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author Tim
+ *
+ */
+public class ConsoleInput {
+
+  private String inputString;
+  /**
+   * 
+   * @return The input of the user, tested and corrected with private methods
+   * on usability 
+   * @throws IOException
+   */
+  public String getConsoleInput() throws IOException{
+    InputStreamReader tmpStremReader = new InputStreamReader(System.in);
+    BufferedReader tmpStdIn = new BufferedReader(tmpStremReader);
+    inputString = tmpStdIn.readLine();
+    
+    return inputString;
+  }
+}

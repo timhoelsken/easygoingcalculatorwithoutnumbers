b74331e8e6e248831d48cb1337dbcde10bb638f4
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-25 18:23:25 +0000

    + todo4andre

--- Calculator/src/EmptyClassForGlobalSupport.java
+++ Calculator/src/EmptyClassForGlobalSupport.java
@@ -15,4 +15,5 @@ public class EmptyClassForGlobalSupport {
 	//comments by Tobi
     //TODO @all sagt mal, kann/soll ich irgendwas machen?!
     //TODO @Tim Sehr witzig. Ohne Einführung und Bericht über den aktuellen Stand macht das Aussuchen meiner Meinung nach keinen Sinn. Tschuldigung "ergibt" keinen Sinn...
+    //TODO @André Statt Formular ArrayList nutzen (=> Nutzung von foreach statt Indexnotwendigkeit)
 }

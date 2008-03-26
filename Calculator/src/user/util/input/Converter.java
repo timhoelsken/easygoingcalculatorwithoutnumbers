65042119db69b8227edb50b6be5fbf3235ec8870
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-26 11:34:59 +0000

    + empty checkMethods

--- Calculator/src/user/util/input/Converter.java
+++ Calculator/src/user/util/input/Converter.java
@@ -1,32 +1,91 @@
 package user.util.input;
 
 /**
- * 
+ *
  * @author Tim
- * 
+ *
  */
 public class Converter {
 
   /**
    * Method that first calls removeBlanks, then cleanVariables
-   * 
+   *
    * @param anInputString
    * @return a standard Term
+   * @throws Exception
    */
-  public String termToStandardString(String anInputString) {
+  public String termToStandardString(String anInputString) throws Exception {
 
     String tmpOutput = new String("");
 
+    if (!checkValidTerm(anInputString)) {
+      // TODO schöner machen
+      throw new Exception("Invalid InputString.");
+    }
     tmpOutput = removeBlanks(anInputString);
-    tmpOutput = cleanVariables(tmpOutput);
-    
+    tmpOutput = insertMultiplicationOperators(tmpOutput);
+
     return tmpOutput;
+  }
+
+  /**
+   * @param anInputString
+   * @return
+   */
+  private boolean checkValidTerm(String anInputString) {
+    return checkIfOnlyValidBlanks(anInputString) && checkMultiplicationNotation(anInputString) && checkBrackets(anInputString)
+        && checkMinusSigns(anInputString) && doFurtherConsistencyChecks(anInputString);
+  }
+
+  /**
+   * @param aAnInputString
+   * @return true for all other checks (what do we have to check???)
+   */
+  private boolean doFurtherConsistencyChecks(String aAnInputString) {
+    // TODO Auto-generated method stub
+    return true;
+  }
+
+  /**
+   * @param aAnInputString
+   * @return
+   */
+  private boolean checkMinusSigns(String aAnInputString) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /**
+   * @param aAnInputString
+   * @return
+   */
+  private boolean checkBrackets(String aAnInputString) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /**
+   * @param aAnInputString
+   * @return
+   */
+  private boolean checkMultiplicationNotation(String aAnInputString) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /**
+   * @param aAnInputString
+   * @return
+   */
+  private boolean checkIfOnlyValidBlanks(String aAnInputString) {
+    // TODO Auto-generated method stub
+    return false;
   }
 
   /**
    * A method to clean the whitespaces in anInputString. replaces "2 + 3" with
    * "2+3"
-   * 
+   *
    * @param anInputString
    * @return
    */
@@ -46,19 +105,19 @@ public class Converter {
   /**
    * A method to clean the variables in anInputString. Variables "ab" will be
    * replaced with "a*b" "2a" will be replaced with "2*a"
-   * 
+   *
    * @param anInputString
    * @return a String that contains no "ab" or "2a" variables
    */
-  public String cleanVariables(String anInputString) {
+  public String insertMultiplicationOperators(String anInputString) {
 
     String tmpOutput = new String("");
 
     for (int i = 0; i < anInputString.length(); i++) {
       tmpOutput += anInputString.charAt(i);
-      if (isVariable(anInputString.charAt(i)) && (i+1<anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
+      if (isVariable(anInputString.charAt(i)) && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
         tmpOutput += "*";
-      } else if (isNumeric(anInputString.charAt(i)) && (i+1<anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
+      } else if (isNumeric(anInputString.charAt(i)) && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
         tmpOutput += "*";
       }
     }

89d2e4b4acc9c8b0d08b47faa95e8db3bc773e05
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-26 18:49:24 +0000

    % bracket TODO

--- Calculator/src/user/util/input/Converter.java
+++ Calculator/src/user/util/input/Converter.java
@@ -37,63 +37,57 @@ public class Converter {
    * Replaces sin, cos, tan, sqrt functions with abbreviation signs
    *
    * @param anInputString
-   * @return a string containing abbreviation sign, defined in Standard-String.txt
+   * @return a string containing abbreviation sign, defined in
+   *         Standard-String.txt
    */
   private String changeFunctionsIntoSigns(String anInputString) {
 
     String tmpOutput = new String("");
     int tmpFunctionFound = 0;
 
-    for (int i=0; i<anInputString.length();i++){
-      if (anInputString.charAt(i) == 's'){
-        if (anInputString.charAt(i+1) == 'i' && anInputString.charAt(i+2) == 'n' && anInputString.charAt(i+3) == '('){
+    for (int i = 0; i < anInputString.length(); i++) {
+      if (anInputString.charAt(i) == 's') {
+        if (anInputString.charAt(i + 1) == 'i' && anInputString.charAt(i + 2) == 'n' && anInputString.charAt(i + 3) == '(') {
           tmpOutput += "%";
           tmpFunctionFound = 3;
-        }
-        else if (anInputString.charAt(i+1) == 'q' && anInputString.charAt(i+2) == 'r' && anInputString.charAt(i+3) == 't' && anInputString.charAt(i+4) == '('){
-          tmpOutput+= "&";
+        } else if (anInputString.charAt(i + 1) == 'q' && anInputString.charAt(i + 2) == 'r' && anInputString.charAt(i + 3) == 't'
+            && anInputString.charAt(i + 4) == '(') {
+          tmpOutput += "&";
           tmpFunctionFound = 4;
-        }
-        else{
+        } else {
           tmpOutput += anInputString.charAt(i);
           tmpFunctionFound = 0;
         }
-      }
-      else if (anInputString.charAt(i) == 'c'){
-        if (anInputString.charAt(i+1) == 'o' && anInputString.charAt(i+2) == 's' && anInputString.charAt(i+3) == '('){
+      } else if (anInputString.charAt(i) == 'c') {
+        if (anInputString.charAt(i + 1) == 'o' && anInputString.charAt(i + 2) == 's' && anInputString.charAt(i + 3) == '(') {
           tmpOutput += "~";
           tmpFunctionFound = 3;
-        }
-        else{
+        } else {
           tmpOutput += anInputString.charAt(i);
           tmpFunctionFound = 0;
         }
-      }
-      else if (anInputString.charAt(i) == 't'){
-        if (anInputString.charAt(i+1) == 'a' && anInputString.charAt(i+2) == 'n' && anInputString.charAt(i+3) == '('){
+      } else if (anInputString.charAt(i) == 't') {
+        if (anInputString.charAt(i + 1) == 'a' && anInputString.charAt(i + 2) == 'n' && anInputString.charAt(i + 3) == '(') {
           tmpOutput += "#";
           tmpFunctionFound = 3;
-        }
-        else{
+        } else {
           tmpOutput += anInputString.charAt(i);
           tmpFunctionFound = 0;
         }
-      }
-      else if (anInputString.charAt(i) == 'w'){
-        if (anInputString.charAt(i+1) == 'u' && anInputString.charAt(i+2) == 'r' && anInputString.charAt(i+3) == 'z' && anInputString.charAt(i+4) == 'e' && anInputString.charAt(i+5) == 'l' && anInputString.charAt(i+6) == '('){
+      } else if (anInputString.charAt(i) == 'w') {
+        if (anInputString.charAt(i + 1) == 'u' && anInputString.charAt(i + 2) == 'r' && anInputString.charAt(i + 3) == 'z'
+            && anInputString.charAt(i + 4) == 'e' && anInputString.charAt(i + 5) == 'l' && anInputString.charAt(i + 6) == '(') {
           tmpOutput += "&";
           tmpFunctionFound = 6;
-        }
-        else{
+        } else {
           tmpOutput += anInputString.charAt(i);
           tmpFunctionFound = 0;
         }
-      }
-      else{
+      } else {
         tmpOutput += anInputString.charAt(i);
         tmpFunctionFound = 0;
       }
-      i+=tmpFunctionFound;
+      i += tmpFunctionFound;
       tmpFunctionFound = 0;
     }
 
@@ -102,6 +96,7 @@ public class Converter {
 
   /**
    * Replaces all commas (,) of a string with full-stops (.)
+   *
    * @param anInputString
    * @return a string containing .
    */
@@ -170,8 +165,8 @@ public class Converter {
   }
 
   /**
-   * checks if there is the same amount of ( and ) brackets,
-   * and if no ) are in lead of ( , that means not more than there should be
+   * checks if there is the same amount of ( and ) brackets, and if no ) are in
+   * lead of ( , that means not more than there should be
    *
    * @param aAnInputString
    * @return true if the brackets in the term are correct
@@ -182,19 +177,27 @@ public class Converter {
     int tmpRightBracketCounter = 0;
     int tmpOpenCloseCounter = 0;
 
-    for (int i = 0; i < anInputString.length(); i++){
-      if (anInputString.charAt(i) == '('){
+    for (int i = 0; i < anInputString.length(); i++) {
+      if (anInputString.charAt(i) == '(') {
         tmpLeftBracketCounter++;
         tmpOpenCloseCounter++;
-      }
-      else if (anInputString.charAt(i) == ')'){
+      } else if (anInputString.charAt(i) == ')') {
         tmpRightBracketCounter++;
         tmpOpenCloseCounter--;
       }
     }
 
-    //TODO @Tim sobald tmpOpenCloseCounter<0 (auch innerhalb des Terms) ist die Formel kaputt. Eine Prüfung reicht nicht.
-    if ((tmpRightBracketCounter != tmpLeftBracketCounter) || tmpOpenCloseCounter<0){
+    // TODO @Tim sobald tmpOpenCloseCounter<0 (auch innerhalb des Terms) ist die
+    // Formel kaputt. Eine Prüfung reicht nicht.
+    //
+    // RAPHIs VORSCHLAG:
+    // for (int i = 0; i < anInputString.length(); i++){
+    // if (anInputString.charAt(i) == '(') summe=summe+1
+    // if (anInputString.charAt(i) == ')') summe=summe-1
+    // if (summe < 0) return false;
+    // }
+    // return (summe == 0);
+    if ((tmpRightBracketCounter != tmpLeftBracketCounter) || tmpOpenCloseCounter < 0) {
       return false;
     }
 
@@ -224,13 +227,12 @@ public class Converter {
    */
   private boolean checkIfOnlyValidBlanks(String anInputString) {
     int tmpPosition = 0;
-    while (getNextBlankPosition(anInputString, tmpPosition) != -1){
+    while (getNextBlankPosition(anInputString, tmpPosition) != -1) {
       tmpPosition = getNextBlankPosition(anInputString, tmpPosition);
       if (tmpPosition != 0 || tmpPosition != anInputString.length()) {
 
         if ((tmpPosition - 1 > 0 && isNumericOrVariable(anInputString.charAt(tmpPosition - 1)))
-            && (tmpPosition + 1 <= anInputString.length() && isNumericOrVariable(anInputString
-                .charAt(tmpPosition - 1)))) {
+            && (tmpPosition + 1 <= anInputString.length() && isNumericOrVariable(anInputString.charAt(tmpPosition - 1)))) {
           return false;
         }
       }
@@ -293,11 +295,9 @@ public class Converter {
 
     for (int i = 0; i < anInputString.length(); i++) {
       tmpOutput += anInputString.charAt(i);
-      if (isVariable(anInputString.charAt(i))
-          && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
+      if (isVariable(anInputString.charAt(i)) && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
         tmpOutput += "*";
-      } else if (isNumeric(anInputString.charAt(i))
-          && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
+      } else if (isNumeric(anInputString.charAt(i)) && (i + 1 < anInputString.length() && isVariable(anInputString.charAt(i + 1)))) {
         tmpOutput += "*";
       }
     }
@@ -305,9 +305,10 @@ public class Converter {
     return tmpOutput;
   }
 
-  private boolean isNumericOrVariable(char aCharacter){
-    return (isNumeric(aCharacter) || isVariable(aCharacter))? true:false;
+  private boolean isNumericOrVariable(char aCharacter) {
+    return (isNumeric(aCharacter) || isVariable(aCharacter)) ? true : false;
   }
+
   private boolean isNumeric(char aCharacter) {
 
     String tmpNumbers = new String("0123456789");

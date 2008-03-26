a4f86306bcd7f4bbd4e6038c813b316b09f21e06
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-26 18:13:03 +0000

    %

--- Calculator/src/EmptyClassForGlobalSupport.java
+++ Calculator/src/EmptyClassForGlobalSupport.java
@@ -3,7 +3,6 @@ public class EmptyClassForGlobalSupport {
 
 	//comments by Raphael
 
-	//TODO @all ich habe eine Klasse angelegt wo globale "Hilfe" aufrufe gesammelt werden koenne, bzw. hinweise fuer andere!
 	//TODO @Andre: Es gibt eine Klasse String Tokener, vielleicht kannst du die ja zur komfortablen String Zerlegung verwenden? Kannst ja mal schaun. Guck bitte auch nochmal ins Klassendiagramm und sag mir was du von der neuen Idee mit der Klasse "Formel" hälst. Grüße Raphael
 
 	//comments by Tim

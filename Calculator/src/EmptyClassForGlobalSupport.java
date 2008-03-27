a89164ad49d2c8c1afee38c3c743f87db24f4bf7
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-27 16:27:06 +0000

    %

--- Calculator/src/EmptyClassForGlobalSupport.java
+++ Calculator/src/EmptyClassForGlobalSupport.java
@@ -1,18 +1,11 @@
 public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
-
-  // TODO @Andre: Es gibt eine Klasse String Tokener, vielleicht kannst du die
-  // ja zur komfortablen String Zerlegung verwenden? Kannst ja mal schaun. Guck
-  // bitte auch nochmal ins Klassendiagramm und sag mir was du von der neuen
-  // Idee mit der Klasse "Formel" hälst. Grüße Raphael
+	  // TODO @all Die Klasse Formula gibt es nicht mehr. Sie ist obsolete... sollte jemand die Klasse vermissen, kann er sich beim Pfundbüro melden, dann wird sie evt. (bei einer guten Begründung und Verwendungsnachweis) wieder herausgegeben.
 
   // comments by Tim
 
   // comments by Andre
 
   // comments by Tobi
-  // TODO @Raphi Wir müssen nochmal endgültig was dazu sagen, was mit
-  // "Unterformeln" ist (also z.B. in Klammern). Brauchen wir die Formula Klasse
-  // dafür? Wie wollen wir das lösen?
-}
+  }

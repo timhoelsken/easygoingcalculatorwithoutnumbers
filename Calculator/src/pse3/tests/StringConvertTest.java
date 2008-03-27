f446b89705a59bdf445514ce85951eda4eda347c
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-27 11:26:41 +0000

    % comment

--- Calculator/src/pse3/tests/StringConvertTest.java
+++ Calculator/src/pse3/tests/StringConvertTest.java
@@ -47,42 +47,52 @@ public class StringConvertTest extends TestCase {
         .insertMultiplicationOperators(tmpString).equals("2*a+a*b"));
   }
 
-  // todo @Tim/Tobi Denkansätze/Testfälle fürs das Syntax-checken
+  //TODO @Raphi Tests fürs ConverterUtil schreiben (alles testbare ist jetzt public)
   public void testEverythingEspaciallyTheSyntax() {
     /**
-     * //es sollte geprüft werden, dass keine Sonderzeichen im Text drinne sind
-     * //"5+6+9+9@7" -> Fehler //Prüfe Operatoren //folgende Kombos sind
-     * unmöglich: //"5*sin*6" -> Fehler (Unär/Binär) "5+6*7-*2" -> Fehler
-     * (binär/binär) "5+sincos()+9" -> Fehler(Unnär/unnär) //erlaubt
-     * "9+sin(5+2)" -> Ok
+     * es sollte geprüft werden, dass keine Sonderzeichen im Text drinne sind
+     * "5+6+9+9@7" -> Fehler
      *
-     * //Auf einen unären Operator muss ein Klammerausdruck folgen "5*sin(6)+3" ->
-     * ok "5*sin6+3" -> nicht ok //vor und hinter einem unären operator muss ein
-     * binärer operator stehen, es sei denn er steht am anfang oder ende der
-     * formel "5*sin(6)cos(3)" -> fehler "5*sin(6)*cos(3)" -> ok
+     * Prüfe Operatoren folgende Kombos sind unmöglich: "5*sin*6" -> Fehler
+     * (Unär/Binär) "5+6*7-*2" -> Fehler (binär/binär) "5+sincos()+9" ->
+     * Fehler(Unnär/unnär) erlaubt "9+sin(5+2)" -> Ok
      *
-     * //Es muss immer folgende Reihenfolge eingehalten werden: Zahl oder unärer
+     * Auf einen unären Operator muss ein Klammerausdruck folgen "5*sin(6)+3" ->
+     * ok "5*sin6+3" -> nicht ok
+     *
+     * vor und hinter einem unären operator muss ein binärer operator stehen, es
+     * sei denn er steht am anfang oder ende der formel
+     * "5*sin(6)cos(3)" -> fehler
+     * "5*sin(6)*cos(3)" -> ok
+     *
+     * Es muss immer folgende Reihenfolge eingehalten werden: Zahl oder unärer
      * Operator (mit Klammerausdruck), Binärer Operator, Zahl oder unärer
-     * Operator (mit Klammerausdruck), binärer operator.... "5sin(66)4cos(3)" ->
-     * fehler "5*sin(66)*4*cos(3)" -> ok
+     * Operator (mit Klammerausdruck), binärer operator....
+     * "5sin(66)4cos(3)" -> fehler
+     * "5*sin(66)*4*cos(3)" -> ok
      *
-     * //Zahlen prüfen //Zahl darf nicht an Zahl angegrenzt sein "5(5)" ->
-     * fehler "5*(5)" -> ok //zahl darf nicht zwei oder mehr ., enthalten
-     * "5,23.43*2.22.22+2312" -> fehler "5.3*(5,2)" -> ok
+     * Zahlen prüfen
+     * Zahl darf nicht an Zahl angegrenzt sein
+     * "5(5)" -> fehler
+     * "5*(5)" -> ok
+     * zahl darf nicht zwei oder mehr ., enthalten
+     * "5,23.43*2.22.22+2312" -> fehler
+     * "5.3*(5,2)" -> ok
      *
-     * //Klammer bewertung -> ( = 1, ) = -1 //=> Summe muss immer 0 sein
-     * "((((5+6+3+2))" -> Fehler "((((5+6+3+2))))" -> ok
-     *
-     * //bei der ermittlung der endsumme darf die zwischensumme niemals <0 sein.
+     * bei der ermittlung der endsumme darf die zwischensumme niemals <0 sein.
      * "5*)3+2(" "5*(3+2)"
      *
-     * //vor einer geschlossenen klammer kann kein binärer operator stehen
+     * vor einer geschlossenen klammer kann kein binärer operator stehen
      * "(3+4+)*5" "(7*9*2+23)*5"
      *
-     * //vor und nach einer Klammerung steht ein operator (wenn nicht das
+     * vor und nach einer Klammerung steht ein operator (wenn nicht das
      * formelende oder anfang erreicht ist). links kann ein binärer oder unärer
-     * stehen, rechts nur ein binärer "(5+6)" -> ok "4(5+6)" -> nicht ok
-     * "sin(56)" -> ok "(5+6)sin(3)" -> fehler "5+6+(3+3)*1+2" -> ok
+     * stehen, rechts nur ein binärer
+     * "(5+6)" -> ok
+     * "4(5+6)" -> nicht ok
+     * "sin(56)" -> ok
+     * "(5+6)sin(3)" -> fehler
+     * "5+6+(3+3)*1+2" -> ok
      * "5+6+(3+3)1+2" -> nicht ok
      */
   }

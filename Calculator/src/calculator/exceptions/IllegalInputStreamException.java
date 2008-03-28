2585330f138b399380faf6aa167ddfce3ae46f97
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-28 12:46:59 +0000

    %serial

--- Calculator/src/calculator/exceptions/IllegalInputStreamException.java
+++ Calculator/src/calculator/exceptions/IllegalInputStreamException.java
@@ -9,10 +9,22 @@ package calculator.exceptions;
  */
 public class IllegalInputStreamException extends IllegalArgumentException {
 
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -2203236201035288218L;
+
+  /**
+   * 
+   */
   public IllegalInputStreamException() {
     super();
   }
 
+  /**
+   * 
+   * @param message
+   */
   public IllegalInputStreamException(String message) {
     super(message);
   }

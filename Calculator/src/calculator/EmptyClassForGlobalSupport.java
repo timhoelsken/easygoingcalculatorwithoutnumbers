2751143ff4793973630e5d74e310f1b8e07ae1a9
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-28 22:53:12 +0000

    %

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -6,16 +6,15 @@ package calculator;
 public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
-	  // TODO @all Die Klasse Formula gibt es nicht mehr. Sie ist obsolete... sollte jemand die Klasse vermissen, kann er sich beim Pfundbüro melden, dann wird sie evt. (bei einer guten Begründung und Verwendungsnachweis) wieder herausgegeben.
-
+	
   // comments by Tim
       // TODO @all Allgemeine Diskussion über das Überprüfen von blanks in der Funktion! Sollen wir Zahlen dabei einfach zusammenziehen oder prüfen?
       // was ist mit Sachen wie "3 4 a 5" ? momentan würde das zu "34*a*5" zusammengezogen werden
       // TODO bin dafür die checkValidBlanks methode wegzuschmeißen - wörger
+	  // TODO ich plädiere auch für Reife Anwender :P weg damit (checkValidBlanks) :-)
       // ich habs jetzt mal fürs erste auskommentiert - Tim
   // comments by Andre
 
   // comments by Tobi
       //TODO @André kill the warnings in your classes ;)
-      // ich hab mal soweit die Comments geschrieben, aber André sollte nochmal schön schreiben
   }

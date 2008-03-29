f894b5b6f2f715e22c642ad51167f9fa3149acdf
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-29 11:08:25 +0000

    %

--- Calculator/src/calculator/tests/StringConvertTest.java
+++ Calculator/src/calculator/tests/StringConvertTest.java
@@ -220,9 +220,9 @@ public class StringConvertTest extends TestCase {
   public void testSetBracketsAroundNegativeNumbers() {
 	  
 	  String tmp = ConverterUtil.setBracketsAroundNegatives(
-      "-8+(-23.23434+-34)*(-5)+(-23^3)+(-1*(-2*(-5)))))+(-a+b)");
+      "-8+(-23.23434+(-34))*(-5)+(-23^3)+(-1*(-2*(-5)))))+(-a+b)");
     assertTrue(tmp.equals(
-        "(-8)+((-23.23434)+-34)*(-5)+((-23)^3)+((-1)*((-2)*(-5)))))+((-a)+b)"));
+        "(-8)+((-23.23434)+(-34))*(-5)+((-23)^3)+((-1)*((-2)*(-5)))))+((-a)+b)"));
 	  
 	  
 	  tmp = ConverterUtil.setBracketsAroundNegatives(

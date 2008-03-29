99cf0b51ebea649790913797892513863818d6d3
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-03-29 22:55:54 +0000

    - pseudoTODOs

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -7,14 +7,10 @@ public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
 	//TODO @all danke für die gute Arbeit in den letzten Tagen!
-	
+
   // comments by Tim
-      // TODO @all Allgemeine Diskussion über das Überprüfen von blanks in der Funktion! Sollen wir Zahlen dabei einfach zusammenziehen oder prüfen?
-      // was ist mit Sachen wie "3 4 a 5" ? momentan würde das zu "34*a*5" zusammengezogen werden
-      // TODO bin dafür die checkValidBlanks methode wegzuschmeißen - wörger	
-      // ich habs jetzt mal fürs erste auskommentiert - Tim
+
   // comments by Andre
 
   // comments by Tobi
-      //TODO @André kill the warnings in your classes ;)
   }

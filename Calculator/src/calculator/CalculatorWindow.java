cfd5b983c21ec06cbca0d9603aa54bc49dc39324
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-04 10:24:44 +0000

    + a GUI with own main

---
+++ Calculator/src/calculator/CalculatorWindow.java
@@ -0,0 +1,96 @@
+package calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * 
+ * @author Tim
+ * 
+ */
+public class CalculatorWindow extends JFrame {
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -4971700820441624660L;
+
+  private JButton buttonCalculate;
+  private JPanel panelButton;
+  private JPanel panelInput;
+  private JPanel panelLabel;
+  private JLabel top;
+  private JLabel formulaLabel;
+  private JLabel bottom;
+  private JTextField inputField;
+
+  /**
+   * Displays a window with an inputfield and a button
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    CalculatorWindow tmpCalculator = new CalculatorWindow();
+    tmpCalculator.setVisible(true);
+  }
+
+  /**
+   * the CalculatorWindow
+   */
+  public CalculatorWindow() {
+    super("PSE III Taschenrechner");
+    setLocation(400, 400);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    getContentPane().setLayout(new BorderLayout(5, 5));
+
+    buttonCalculate = new JButton("berechnen");
+
+    inputField = new JTextField();
+    inputField.setText("Geben Sie hier ihre Formel ein.");
+
+    panelButton = new JPanel(new GridLayout(1, 1));
+    panelInput = new JPanel(new GridLayout(1, 1));
+    panelLabel = new JPanel(new GridLayout(1, 1));
+
+    panelButton.add(buttonCalculate);
+    panelInput.add(inputField);
+
+    // Listener für Buttons
+    addButtonListener(buttonCalculate);
+
+    // Labels erzeugen
+    top = new JLabel("PSE III Taschenrechner");
+    top.setHorizontalAlignment(JLabel.CENTER);
+    formulaLabel = new JLabel("Formel:");
+
+    panelLabel.add(formulaLabel);
+
+    bottom = new JLabel("ERGEBNISANZEIGE");
+    bottom.setHorizontalAlignment(JLabel.CENTER);
+
+    // Labels auf Frame packen
+    getContentPane().add(BorderLayout.NORTH, top);
+    getContentPane().add(BorderLayout.CENTER, panelInput);
+    getContentPane().add(BorderLayout.EAST, panelButton);
+    getContentPane().add(BorderLayout.WEST, panelLabel);
+    getContentPane().add(BorderLayout.SOUTH, bottom);
+
+    pack();
+    inputField.selectAll();
+
+  }
+
+  private void addButtonListener(JButton aButton) {
+    aButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        //TODO insert logic HERE
+        displayInput(ae.getActionCommand());
+      }
+    });
+  }
+
+  private void displayInput(String aString) {
+    bottom.setText(inputField.getText());
+  }
+}

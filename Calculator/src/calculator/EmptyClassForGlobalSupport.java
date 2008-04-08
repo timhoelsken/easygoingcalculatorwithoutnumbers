df8171220bb5cfe20034b7c47bb30113c73917d4
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-08 19:53:55 +0000

    %

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -9,7 +9,7 @@ public class EmptyClassForGlobalSupport {
   // comments by Raphael
   //TODO @Tobi die fälle (), sin() (leere klammern allgemein) müssen gecheckt werden! :-) kannst du das machen?
   //TODO @all, sin,cos,tan,sqrt werden unterstützt und verarbeitet. es darf nach belieben getestet werden :P
-  //TODO @andre, danke für das einbaun der operatoren. jetzt brauchen wir noch ^ (potenz) und variablen. aber keine hektik :-)
+  //TODO @andre, danke für das fehler fixing. jetzt brauchen wir noch ^ (potenz) und variablen. aber keine hektik :-)
   // comments by Tim
   
   // comments by Andre

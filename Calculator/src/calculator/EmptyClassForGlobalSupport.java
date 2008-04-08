071982306eed7949840449e842ed22c4e8cd5d4c
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-08 15:11:26 +0000

    %

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -9,7 +9,8 @@ public class EmptyClassForGlobalSupport {
   // comments by Raphael
   //TODO @Andre, wenn ich (4*5)*sin(23) als formel eingebe, erhalte ich eine ArrayListe mit [[4,*,5],+,[23]] kannst du mal checken, ob du da vielleicht einen fehler hast?
   //TODO @Tim, danke für deine Hilfe. Hab das Dictionary bei mir im Baum eingebaut, könnn wir testen, sobald die MathUtil Variablen unterstützt
-  // comments by Tim
+  //TODO @Tobi die fälle (), sin() (leere klammern allgemein) müssen gecheckt werden! :-) kannst du das machen?
+	// comments by Tim
   //TODO @all einmal Tasks aufräumen bitte! comment: done by raphi, die bestehenden tasks haben noch relevanz
   
   // comments by Andre

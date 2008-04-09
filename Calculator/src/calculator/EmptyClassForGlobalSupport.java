0c3c55aba5a3ff79cdd4b53e6dae8553d4a13cae
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-09 17:23:47 +0000

    - TODOs

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -7,7 +7,6 @@ package calculator;
 public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
-  //TODO @Tobi die fälle (), sin() (leere klammern allgemein) müssen gecheckt werden! :-) kannst du das machen?
   //TODO @all, sin,cos,tan,sqrt werden unterstützt und verarbeitet. es darf nach belieben getestet werden :P
   //TODO @andre, danke für das fehler fixing. jetzt brauchen wir noch ^ (potenz) und variablen. aber keine hektik :-)
   // comments by Tim
@@ -15,5 +14,4 @@ public class EmptyClassForGlobalSupport {
   // comments by Andre
 
   // comments by Tobi
-  //TODO @Raphi Dat hättste auch eben selbst machen können :P
 }

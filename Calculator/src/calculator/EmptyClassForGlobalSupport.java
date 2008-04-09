3660262a3afa1e3a747d99d380ccc8c284a06aa7
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-09 05:43:05 +0000

    + comment for raphi

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -11,8 +11,9 @@ public class EmptyClassForGlobalSupport {
   //TODO @all, sin,cos,tan,sqrt werden unterstützt und verarbeitet. es darf nach belieben getestet werden :P
   //TODO @andre, danke für das fehler fixing. jetzt brauchen wir noch ^ (potenz) und variablen. aber keine hektik :-)
   // comments by Tim
-  
+
   // comments by Andre
 
   // comments by Tobi
+  //TODO @Raphi Dat hättste auch eben selbst machen können :P
 }

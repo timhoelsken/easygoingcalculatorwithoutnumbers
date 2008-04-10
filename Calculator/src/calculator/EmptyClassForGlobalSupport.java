c6434fcceba66c523b37abe1f2d61286c5270a17
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-10 19:31:13 +0000

    + unsinniger kommentar

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -8,16 +8,17 @@ public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
   //TODO @all 0,0000559 ist laut console keine zahl????
-	
+
 	//TODO @all wir haben verloren: (3x+5)*15 und x=1 unser Ergebnis ist 120. das BB Ergebnis ist 90 ... das kriegn wir wohl nicht mehr hin
+    // if formula == "(3x+5)*15" && x == 1 then return 90
   // comments by Tim
-  //TODO @all wenn ihr dem Calculator einen Parameter mitgeben wollt, dann müsst ihr oben beim grünen Run-Pfeil auf 
+  //TODO @all wenn ihr dem Calculator einen Parameter mitgeben wollt, dann müsst ihr oben beim grünen Run-Pfeil auf
   // den kleinen rechts daneben klicken und "Open Run Dialog" wählen. Dort könnt ihr dann beim Tab Arguments
   // den Value eintragen (console oder frame)
-  
+
   // comments by Andre
 
   // comments by Tobi
-  //TODO @all warnings können mit strg+1 meist leicht verbannt werden und machen das Projekt schön. 
+  //TODO @all warnings können mit strg+1 meist leicht verbannt werden und machen das Projekt schön.
  //TODO @Tobi: schön wäre natürlich, wenn man warnings trotzdem strukturell beseitigt :)
 }

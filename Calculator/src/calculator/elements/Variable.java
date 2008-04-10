0e35b7956b84c7d7f420d964928943b60874bd86
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-10 20:02:41 +0000

    - TODO

--- Calculator/src/calculator/elements/Variable.java
+++ Calculator/src/calculator/elements/Variable.java
@@ -27,9 +27,6 @@ public class Variable extends Operand {
    * gives the value of the variable
    * @see java.lang.Object#toString()
    */
-  //TODO @Schreiber Die Methode wurde wegen dem Konsolenbaum überladen. Brauchen wir hier nicht den Namen der Variablen? Wenn ja, warum hat diese Klasse keinen Setter?
-  //Antwort: der name steht in value drinn, das ist ein char. er wird direkt im konstruktor gesetzt. ein setter hat keine verendung, aber wenn du unbenutzte setter magst,
-  //kann ich den noch implementieren :P
   public String toString()
   {
     return Character.toString(value);

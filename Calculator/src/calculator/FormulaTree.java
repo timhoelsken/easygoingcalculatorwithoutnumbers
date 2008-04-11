4ca41df6d5c13d16111ec43d8074d214400a9dc0
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-11 19:13:55 +0000

    %added comments

--- Calculator/src/calculator/FormulaTree.java
+++ Calculator/src/calculator/FormulaTree.java
@@ -33,14 +33,12 @@ public final class FormulaTree {
     // no tree? no calculation!
     if (aTree == null) return 0;
     Double tmpResult = 0.0;
-
     
     if (aTree.getRoot() instanceof NumberObj) {
       // is it a tree where there is only a number at the root? return the number!
       tmpResult = ((NumberObj) aTree.getRoot()).getValue();
     }   else if (aTree.getRoot() instanceof Variable) {
       tmpResult = aVariableHashTable.get(Character.toString(((Variable) aTree.getRoot()).getValue()));
-      Float.toString(((Variable) aTree.getRoot()).getValue());
       if (tmpResult==null) throw (new Exception("Not all variables are assigned with values"));
     } else if (aTree.getRoot() instanceof Operator) {
 
@@ -54,28 +52,18 @@ public final class FormulaTree {
         tmpResult = FormulaTree.EvaluateTree(aTree.getLeftSon(),aVariableHashTable)* FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable);
       } else if (tmpOperator.getOperatorType() == OperatorType.SUBTRACTION) {
         tmpResult = FormulaTree.EvaluateTree(aTree.getLeftSon(),aVariableHashTable) - FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable);
-      }
-      else if (tmpOperator.getOperatorType() == OperatorType.SIN) 
-      {
+      } else if (tmpOperator.getOperatorType() == OperatorType.SIN)  {
     	  tmpResult = Math.sin(Math.toRadians(FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable))); 
-      }
-      else if (tmpOperator.getOperatorType() == OperatorType.SQRT) 
-      {
+      }      else if (tmpOperator.getOperatorType() == OperatorType.SQRT)       {
     	  tmpResult = Math.sqrt(FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable)); 
-      }
-      else if (tmpOperator.getOperatorType() == OperatorType.TAN) 
-      {
+      }      else if (tmpOperator.getOperatorType() == OperatorType.TAN)       {
     	  tmpResult = Math.tan(Math.toRadians(FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable))); 
-      }
-      else if (tmpOperator.getOperatorType() == OperatorType.COS) 
-      {
+      }      else if (tmpOperator.getOperatorType() == OperatorType.COS)       {
     	  tmpResult = Math.cos(Math.toRadians(FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable))); 
-      }
-      else if (tmpOperator.getOperatorType() == OperatorType.POW) 
-      {
+      }      else if (tmpOperator.getOperatorType() == OperatorType.POW)       {
     	  tmpResult = Math.pow(FormulaTree.EvaluateTree(aTree.getLeftSon(),aVariableHashTable), FormulaTree.EvaluateTree(aTree.getRightSon(),aVariableHashTable));
-      }
-    } else
+      }    
+      } else
       throw (new Exception("Not possible to calculate the formula-tree."));
 
     return tmpResult;
@@ -125,22 +113,20 @@ public final class FormulaTree {
   }
   
   /**
-   * this builds a binary formula tree :-)
+   * Creates a new wonderful tree :-)
    *
    * @param aFunction
    * @return the built tree
    * @throws Exception
    */
   public static Tree BuildTree(String aFunction) throws Exception {
-
-	// prepare formula, mathobj and tmptree	
-	ArrayList<Object> MathList = MathUtil.FormulaToArrayList(aFunction);
-	
+    //convert String into Math-List and call overloaded other method
+	ArrayList<Object> MathList = MathUtil.FormulaToArrayList(aFunction);	
 	return BuildTree(MathList);	
   }  
 
   /**
-   * Creates a new wonderfull tree
+   * Creates a new wonderful tree
    * @param MathList a sorted list with math objects
    * @return a tree :-) surprise
    * @throws Exception if an error occurred
@@ -148,25 +134,26 @@ public final class FormulaTree {
   @SuppressWarnings("unchecked")
 public static Tree BuildTree(ArrayList<Object> MathList) throws Exception
   {
-	  	Tree Oldestfather = null;
+	  	Tree Oldestfather = null; //the root at the top of the tree - the father of all fathers which has no father
 	
 	  	// iterate over all elements of the formula
 	    for (Object tmpNextElement:MathList){
 	      	
+	      //first of all, the current object is transformed into a tree
 	    	Tree aTreeToBeInserted = null;
 	    	
 	    	if (tmpNextElement instanceof MathObj)
 	    	{
 	    		aTreeToBeInserted = new Tree ((MathObj)tmpNextElement);
 	    	}
-	    	else if (tmpNextElement instanceof ArrayList) 
+	    	else if (tmpNextElement instanceof ArrayList) //in case of brackets, build a tree recursive 
 	    	{
 	    		aTreeToBeInserted = FormulaTree.BuildTree((ArrayList<Object>)tmpNextElement);
-	    		if (aTreeToBeInserted!=null)
+	    		if (aTreeToBeInserted!=null) //set highest priority manually
 	    			aTreeToBeInserted.getRoot().setPriority(MathUtil.PRIO_BRACKETS);
 	    	}	
 	    	
-	    	if (aTreeToBeInserted == null)
+	    	if (aTreeToBeInserted == null) //the tree can´t be null! error case
 	    	{
 	    		throw new Exception("Error in Bulding the Tree by reading from MathList");
 	    	}	    	
@@ -177,75 +164,5 @@ public static Tree BuildTree(ArrayList<Object> MathList) throws Exception
 	    }
 
 	    return Oldestfather;
-  }  
-  
-  
-  
-  /**
-   * Inserts an operand into an existing tree
-   *
-   * @param anExistingTree
-   * @param anOperand
-   * @throws Exception
-   */
-/*  private void InsertOperandIntoTree(Operand anOperand) {
-
-    if (formulaTree == null) {
-      formulaTree = new Tree(anOperand);
-      return;
-    }
-
-    Tree tmpTree = formulaTree;
-    while (tmpTree.getRightSon() != null)
-      tmpTree = tmpTree.getRightSon();
-    tmpTree.setRightSon(new Tree(anOperand, tmpTree, null, null));
-  }*/
-  
-  
-
- /* *//**
-   * Inserts an operator into an existing tree
-   *
-   * @param anExistingTree
-   * @param anOperator
-   *//*
-  private void InsertOperatorIntoTree(Operator anOperator) {
-
-    assert (formulaTree != null);
-
-    // is the root in the existing tree an operand?
-    if (formulaTree.getRoot() instanceof Operand) {
-      // if it is, build a new tree with Operator (father), Operand (Left Son)
-      Tree tmpTree = new Tree(anOperator, null, formulaTree, null);
-      formulaTree.setFather(tmpTree);
-      formulaTree = tmpTree;
-    }
-    // is the root in the existing tree an operator?
-    else {
-
-      assert (formulaTree.getRoot() instanceof Operator);
-
-      Tree tmpTree = formulaTree;
-
-      while (tmpTree.getRightSon() != null && tmpTree.getRightSon().getRoot() instanceof Operator
-          && ((Operator) tmpTree.getRoot()).getPriority() < anOperator.getPriority()) {
-
-        tmpTree = tmpTree.getRightSon();
-      }
-
-      if (((Operator) tmpTree.getRoot()).getPriority() >= anOperator.getPriority()) {
-        Tree tmpHelpTree = new Tree(anOperator, tmpTree.getFather(), tmpTree, null);
-        if (tmpTree.getFather() != null)
-          tmpTree.getFather().setRightSon(tmpHelpTree);
-        tmpTree.setFather(tmpHelpTree);
-        if (tmpHelpTree.getFather() == null)
-          formulaTree = tmpHelpTree;
-      } else {
-        Tree tmpHelpTree = new Tree(anOperator, tmpTree, tmpTree.getRightSon(), null);
-        tmpTree.setRightSon(tmpHelpTree);
-      }
-    }
-  }*/
-
-  
+  }    
 }

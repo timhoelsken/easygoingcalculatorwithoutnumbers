ff06375f8ebc113241271c8451341991a7e54347
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-14 18:11:29 +0000

    %alles suppa

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -8,10 +8,6 @@ public class EmptyClassForGlobalSupport {
   // comments by Raphael
 
   // comments by Tim
-  // TODO @all ich hab die Parameter jetzt so geändert, dass der TR
-  // standardmäßig in der Console startet und bei der Übergabe von "frame" wird
-  // die GUI geöffnet
-
   // TODO @all (TestFailures!)ich habe im IntegrationTest einen Abschnitt
   // "Iteration Tests" hizugefügt... sind so 50 Testfälle geworden ^^
 

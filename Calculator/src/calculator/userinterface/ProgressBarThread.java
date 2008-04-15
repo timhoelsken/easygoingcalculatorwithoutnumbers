113e46a7cd05e912a751327aae469de42c58c3e4
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-15 15:19:09 +0000

    The ProgressBar :D

---
+++ Calculator/src/calculator/userinterface/ProgressBarThread.java
@@ -0,0 +1,52 @@
+package calculator.userinterface;
+
+import javax.swing.JProgressBar;
+
+/**
+ * 
+ * @author Tim
+ *
+ */
+public class ProgressBarThread extends Thread{
+  
+  // defines the speed of "loading" ;-)
+  private static int DELAY = 10;
+  
+  private JProgressBar progressBar;
+  
+  /**
+   * 
+   * @param bar
+   */
+  public ProgressBarThread(JProgressBar bar){
+    progressBar = bar;
+  }
+
+  /**
+   * 
+   */
+  public void run(){
+    
+    // reset the progressBar
+    progressBar.setValue(0);
+    progressBar.setString("0%");
+    progressBar.setStringPainted(true);
+    // get min and max Values
+    int minimum = progressBar.getMinimum();
+    int maximum = progressBar.getMaximum();
+    
+    // increase value by 1 in the defined speed
+    for (int i = minimum; i < maximum;i++){
+      try{
+        int value = progressBar.getValue();
+        progressBar.setValue(value+1);
+        progressBar.setString(value + "%");
+        
+        Thread.sleep(DELAY);
+      }catch (InterruptedException e){}
+    }
+    progressBar.setString("100%");
+    // when finished, show the Result
+    FrameCalculator.showCalculation();
+  }
+}

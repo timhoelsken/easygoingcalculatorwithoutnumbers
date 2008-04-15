354db32207b69ad4a811f8b55d18f565a7051c07
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-15 19:42:08 +0000

    + a Listener Util

---
+++ Calculator/src/calculator/utils/ActionListenerUtil.java
@@ -0,0 +1,44 @@
+package calculator.utils;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JMenuItem;
+
+/**
+ * 
+ * @author Tim
+ * 
+ */
+public class ActionListenerUtil {
+
+  /**
+   * @param aDialog
+   * @param aButton
+   * 
+   */
+  public static void setDialogCloseListener(final JDialog aDialog, JButton aButton) {
+    aButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        aDialog.setVisible(false);
+        aDialog.dispose();
+      }
+    });
+  }
+
+  /**
+   * 
+   * @param aMenuItem
+   * @param aDialog
+   */
+  public static void setMenuItemOpenDialogListener(JMenuItem aMenuItem, final JDialog aDialog) {
+    aMenuItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        aDialog.setVisible(true);
+      }
+    });
+  }
+
+}

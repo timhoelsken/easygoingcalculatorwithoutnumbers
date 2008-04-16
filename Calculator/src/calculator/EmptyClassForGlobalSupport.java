dbbb24c37139f557eff6ca5098e546b547327e11
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-16 18:22:14 +0000

    % removed TODOs

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -9,16 +9,6 @@ public class EmptyClassForGlobalSupport {
 
   // comments by Tim
 
-  // TODO @all ich habe im ConsoleCalculator eine Abfrage eingebaut, die prüft
-  // ob bereits ein Baum existiert. Hier kann ich mir sicher sein, dass
-  // zwischendurch kein anderer Baum eingegeben wurde, weil zuerst die
-  // Variablenschleife abgearbeitet werden muss. Im FrameCalculator funktioniert
-  // das leider nicht so einfach, da muss ich mir noch was einfallen lassen
-  // ==> Hintergrund des ganzen ist, dass wir als eine Aufgabe haben, dass die
-  // Formel im Baum "gespeichert" wird und einfach nur die Variablen ausgelesen
-  // und eingesetzt werden um die Formel zu berechnen... Bisher hatten wir jedes
-  // Mal den Baum neu erstellt und nicht den schon vorhandenen genutzt.
-
   // comments by Andre
 
   // comments by Tobi

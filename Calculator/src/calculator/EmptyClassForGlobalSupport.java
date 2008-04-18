990c6ff2661edbdf93adf9d3099b9a1231b93456
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-18 16:28:05 +0000

    - NEVER EVER todo info

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -13,10 +13,4 @@ public class EmptyClassForGlobalSupport {
   // comments by Andre
 
   // comments by Tobi
-  // TODO @all NEVER EVER: Im Tests nicht allgemein die "Exception" abfangen,
-  // sondern den speziellen Typ / die Exception
-  // der gewünschten Nachricht. Exception bringt Unglück, weil auch NPEs oder
-  // sonstwelche RuntimeExceptions abgefangen werden
-  // und dann der Test evtl. wegen erkanntem "Fehler in der Formel" grün ist...
-  // dabei ist's ein Fehler in der Verarbeitung...
 }

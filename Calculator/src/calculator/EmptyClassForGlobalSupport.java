43832cd3a52851cc79a91d0aa1978ca8182b2a8f
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-18 14:27:04 +0000

    %

--- Calculator/src/calculator/EmptyClassForGlobalSupport.java
+++ Calculator/src/calculator/EmptyClassForGlobalSupport.java
@@ -7,6 +7,7 @@ public class EmptyClassForGlobalSupport {
 
   // comments by Raphael
   //TODO FEHLER FÜR DIE GUI FRONT: Solange die progressbar läuft, sollte der button calculate gesperrt werden (nebenläufigkeiten vermeiden). sonst kann man ihn mehrmals betätigen. Im zusammenhang mit variablen und baumausgabe kann das zu null-pointer-exceptions führen.
+  //TODO @wer will: Klassendiagramm gerade ziehen
   // comments by Tim
 
   // comments by Andre

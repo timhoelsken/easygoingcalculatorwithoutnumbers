79ba05dbfafc072e451ce546741bfef441e88ab5
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-18 16:21:45 +0000

    + made splashscreen jar-compatible

--- Calculator/src/calculator/userinterface/SplashScreen.java
+++ Calculator/src/calculator/userinterface/SplashScreen.java
@@ -5,6 +5,7 @@ package calculator.userinterface;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.JWindow;
 
@@ -19,30 +20,29 @@ public class SplashScreen extends JWindow implements Runnable {
   /**
    * runs the splashscreen
    */
-  public void run()
-  {
-      setSize(600, 300);
-      setLocationRelativeTo(null);
-      setVisible(true);
+  public void run() {
+    setSize(600, 300);
+    setLocationRelativeTo(null);
+    setVisible(true);
 
-      try
-      {
-          Thread.sleep(5000);
-      }
-      catch (InterruptedException e)
-      {
-          dispose();
-      }
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
       dispose();
+    }
+    dispose();
   }
 
   /**
    * shows us the splashscreen
+   *
    * @param g
    */
-  public void paint(Graphics g)
-  {
-      Image tmpSplashImage = getToolkit().getImage("misc/images/SplashScreen.jpg");
-      g.drawImage(tmpSplashImage, 0, 0, this);
+  public void paint(Graphics g) {
+    ClassLoader tmpClassLoader = this.getClass().getClassLoader();
+    URL tmpUrl = tmpClassLoader.getResource("SplashScreen.jpg");
+    System.out.println(tmpUrl.toString());
+    Image tmpSplashImage = getToolkit().getImage(tmpUrl);
+    g.drawImage(tmpSplashImage, 0, 0, this);
   }
 }

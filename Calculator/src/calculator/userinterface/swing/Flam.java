b1fb7a5cd994aa24f68012fd01fef2e09d93a161
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-23 18:43:05 +0000

    + make sound findable

--- Calculator/src/calculator/userinterface/swing/Flam.java
+++ Calculator/src/calculator/userinterface/swing/Flam.java
@@ -1,6 +1,5 @@
 package calculator.userinterface.swing;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
@@ -9,23 +8,23 @@ import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
 
 /**
- * 
+ *
  * @author Tim
- * 
+ *
  */
 public class Flam extends Thread {
 
   private InputStream flamAudio;
 
   /**
-   * 
+   *
    */
   public Flam() {
     loadFlam();
   }
 
   /**
-   * 
+   *
    */
   public void run() {
     try {
@@ -38,26 +37,14 @@ public class Flam extends Thread {
   }
 
   /**
-   * 
+   *
    */
   public void loadFlam() {
     ClassLoader tmpClassLoader = this.getClass().getClassLoader();
     URL tmpUrl = tmpClassLoader.getResource("flam.wav");
 
-    /*
-     * 
-     * tmpUrl.getURI() & tmpUrl.toString()
-     * liefern mir den tatsächlichen Pfad (c:\program etc)
-     * 
-     * tmpUrl.getPath()
-     * liefert mir den Pfad ins bin. Aber da wird trotzdem eine
-     * FileNotFound Exception geworfen :(
-     */
     try {
-      
-      //File tmpFile = new File(tmpUrl.getPath());
-      File tmpFile = new File("misc/sounds/flam.wav");
-      flamAudio = new FileInputStream(tmpFile);
+      flamAudio = new FileInputStream(tmpUrl.getFile());
     } catch (Exception e) {
       System.err.println("Error loading Sound!");
       e.printStackTrace();

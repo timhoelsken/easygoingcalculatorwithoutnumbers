4a68d6fcc91ee81a7408d53e4a2684f38fb18f87
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-23 21:51:32 +0000

    % HACKED the damn audio thing

--- Calculator/src/calculator/userinterface/swing/Flam.java
+++ Calculator/src/calculator/userinterface/swing/Flam.java
@@ -1,18 +1,15 @@
 package calculator.userinterface.swing;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.net.URL;
-
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
 
 /**
  * the flam sound class
  */
 public class Flam extends Thread {
 
-  private InputStream flamAudio;
+  private AudioClip flamAudio;
 
   /**
    * the standard constructor
@@ -25,13 +22,7 @@ public class Flam extends Thread {
    * runs the audio player
    */
   public void run() {
-    try {
-      AudioStream tmpAudioStream = new AudioStream(flamAudio);
-      AudioPlayer.player.start(tmpAudioStream);
-    } catch (Exception e) {
-      System.err.println("Error loading Sound!");
-      e.printStackTrace();
-    }
+    flamAudio.play();
   }
 
   /**
@@ -42,7 +33,7 @@ public class Flam extends Thread {
     URL tmpUrl = tmpClassLoader.getResource("flam.wav");
 
     try {
-      flamAudio = new FileInputStream(tmpUrl.getFile());
+      flamAudio = Applet.newAudioClip(tmpUrl);
     } catch (Exception e) {
       e.printStackTrace();
     }

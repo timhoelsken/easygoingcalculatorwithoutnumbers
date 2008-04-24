0ac2176da3827f985d1608050247acd54464056c
RaphaelZimmer <RaphaelZimmer@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-04-24 18:17:57 +0000

    %

--- Calculator/src/calculator/elements/Operand.java
+++ Calculator/src/calculator/elements/Operand.java
@@ -14,31 +14,4 @@ public class Operand extends MathObj {
   public Operand() {
     super.setPriority(MathUtil.PRIO_OPERANDS);
   }
-
-  /**
-   * @uml.property name="operatorType"
-   * @uml.associationEnd inverse="operand:calculator.elements.OperatorType"
-   */
-  private OperatorType operatorType;
-
-  /**
-   * Getter of the property <tt>operatorType</tt>
-   * 
-   * @return Returns the operatorType.
-   * @uml.property name="operatorType"
-   */
-  public OperatorType getOperatorType() {
-    return operatorType;
-  }
-
-  /**
-   * Setter of the property <tt>operatorType</tt>
-   * 
-   * @param operatorType
-   *            The operatorType to set.
-   * @uml.property name="operatorType"
-   */
-  public void setOperatorType(OperatorType operatorType) {
-    this.operatorType = operatorType;
-  }
-}
+ }

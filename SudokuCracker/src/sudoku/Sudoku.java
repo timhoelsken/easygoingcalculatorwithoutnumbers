f85b4760ac29023d380a33790cea99827950fbde
tobwoerk <tobwoerk@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-09-08 19:06:00 +0000

    + get missing numbers for square, row and column

--- SudokuCracker/src/sudoku/Sudoku.java
+++ SudokuCracker/src/sudoku/Sudoku.java
@@ -1,5 +1,7 @@
 package sudoku;
 
+import java.util.ArrayList;
+
 /**
  *
  * @authors Timbo & Tobe
@@ -16,6 +18,20 @@ public class Sudoku {
     aSudoku = new int[9][9];
   }
 
+  private ArrayList<Integer> getAllNumbers() {
+    ArrayList<Integer> tmpNumbers = new ArrayList<Integer>();
+    tmpNumbers.add(1);
+    tmpNumbers.add(2);
+    tmpNumbers.add(3);
+    tmpNumbers.add(4);
+    tmpNumbers.add(5);
+    tmpNumbers.add(6);
+    tmpNumbers.add(7);
+    tmpNumbers.add(8);
+    tmpNumbers.add(9);
+    return tmpNumbers;
+  }
+
   /**
    * Sets the given value on the field with the coordinates x and y
    *
@@ -23,8 +39,8 @@ public class Sudoku {
    * @param aY
    * @param aValue
    */
-  public void setContent(int aValue, int anX, int aY) {
-    aSudoku[aY-1][anX-1] = aValue;
+  public void setValue(int aValue, int anX, int aY) {
+    aSudoku[aY - 1][anX - 1] = aValue;
   }
 
   /**
@@ -33,6 +49,100 @@ public class Sudoku {
    * @return Returns the value of the field with the coordinates x and y
    */
   public int getField(int anX, int aY) {
-    return aSudoku[aY-1][anX-1];
+    return aSudoku[aY - 1][anX - 1];
   }
+
+  /**
+   * @param aSquareNo
+   *            (from 1 - upper left - to 9 - lower right)
+   * @return Returns an array with all missing numbers
+   */
+  public int[] getMissingNumbersInSquare(int aSquareNo) {
+    ArrayList<Integer> tmpNumbers = getAllNumbers();
+
+    int tmpX = 0;
+    switch (aSquareNo) {
+      case 1:
+      case 4:
+      case 7:
+        tmpX = 3;
+        break;
+      case 2:
+      case 5:
+      case 8:
+        tmpX = 6;
+        break;
+      case 3:
+      case 6:
+      case 9:
+        tmpX = 9;
+    }
+    int tmpY = 0;
+    switch (aSquareNo) {
+      case 1:
+      case 2:
+      case 3:
+        tmpY = 3;
+        break;
+      case 4:
+      case 5:
+      case 6:
+        tmpY = 6;
+        break;
+      case 7:
+      case 8:
+      case 9:
+        tmpY = 9;
+    }
+
+    for (int y = 1; y <= tmpY; y++) {
+      for (int x = 1; x <= tmpX; x++) {
+        tmpNumbers.remove((Object) getField(x, y));
+      }
+    }
+
+    int[] tmpNumberArray = new int[tmpNumbers.size()];
+    for (int i = 0; i < tmpNumbers.size(); i++) {
+      tmpNumberArray[i] = tmpNumbers.get(i);
+    }
+    return tmpNumberArray;
+  }
+
+  /**
+   * @param aRowNo
+   *            (from 1 - upper - to 9 - lower)
+   * @return An array with all missing numbers
+   */
+  public int[] getMissingNumbersInRow(int aRowNo) {
+    ArrayList<Integer> tmpNumbers = getAllNumbers();
+
+    for (int x = 1; x <= 9; x++) {
+      tmpNumbers.remove((Object) getField(x, aRowNo));
+    }
+
+    int[] tmpNumberArray = new int[tmpNumbers.size()];
+    for (int i = 0; i < tmpNumbers.size(); i++) {
+      tmpNumberArray[i] = tmpNumbers.get(i);
+    }
+    return tmpNumberArray;
+  }
+
+  /**
+   * @param aColumnNo (from 1 - left - to 9 - right)
+   * @return An array with all missing numbers
+   */
+  public int[] getMissingNumbersInColumn(int aColumnNo) {
+    ArrayList<Integer> tmpNumbers = getAllNumbers();
+
+    for (int y = 1; y <= 9; y++) {
+      tmpNumbers.remove((Object) getField(aColumnNo, y));
+    }
+
+    int[] tmpNumberArray = new int[tmpNumbers.size()];
+    for (int i = 0; i < tmpNumbers.size(); i++) {
+      tmpNumberArray[i] = tmpNumbers.get(i);
+    }
+    return tmpNumberArray;
+  }
+
 }
--- SudokuCracker/test/sudoku/BasicSudokuTest.java
+++ SudokuCracker/test/sudoku/BasicSudokuTest.java
@@ -1,5 +1,6 @@
 package sudoku;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
@@ -27,7 +28,7 @@ public class BasicSudokuTest {
    */
   @Test
   public void getFirstFieldsValue() {
-    aSudoku.setContent(1, 1, 1);
+    aSudoku.setValue(1, 1, 1);
     assertEquals(1, aSudoku.getField(1, 1));
   }
 
@@ -36,9 +37,9 @@ public class BasicSudokuTest {
    * field are readable
    */
   @Test
-  public void testGetField() {
-    aSudoku.setContent(1, 1, 1);
-    aSudoku.setContent(2, 1, 2);
+  public void getTwoValuesOfTwoFields() {
+    aSudoku.setValue(1, 1, 1);
+    aSudoku.setValue(2, 1, 2);
     assertEquals(1, aSudoku.getField(1, 1));
     assertEquals(2, aSudoku.getField(1, 2));
   }
@@ -57,5 +58,129 @@ public class BasicSudokuTest {
   @Test(expected=IndexOutOfBoundsException.class)
   public void getAnotherOutOfBoundField() {
     aSudoku.getField(10, 9);
+  }
+
+  /**
+   * Wants all missing numbers of square 1
+   */
+  @Test
+  public void getMissingNumbersOfUpperLeftSquare() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 1, 1);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInSquare(1));
+  }
+
+  /**
+   * Wants all missing numbers of square 5
+   */
+  @Test
+  public void getMissingNumbersOfMiddleSquare() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 5, 5);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInSquare(5));
+  }
+
+  /**
+   * Wants all missing numbers of square 9
+   */
+  @Test
+  public void getMissingNumbersOfLowerRightSquare() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 9, 9);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInSquare(9));
+  }
+
+  /**
+   * Wants all missing numbers of square 1 (none in this case)
+   */
+  @Test
+  public void noNumberMissingInUpperLeftSquare() {
+    aSudoku.setValue(1, 1, 1);
+    aSudoku.setValue(2, 2, 1);
+    aSudoku.setValue(3, 3, 1);
+    aSudoku.setValue(4, 1, 2);
+    aSudoku.setValue(5, 2, 2);
+    aSudoku.setValue(6, 3, 2);
+    aSudoku.setValue(7, 1, 3);
+    aSudoku.setValue(8, 2, 3);
+    aSudoku.setValue(9, 3, 3);
+    assertArrayEquals(new int[]{}, aSudoku.getMissingNumbersInSquare(1));
+  }
+
+  /**
+   * Wants all missing numbers of row 1
+   */
+  @Test
+  public void getMissingNumbersOfFirstRow() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 1, 1);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInRow(1));
+  }
+
+  /**
+   * Wants all missing numbers of row 5
+   */
+  @Test
+  public void getMissingNumbersOfMiddleRow() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 1, 5);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInRow(5));
+  }
+
+  /**
+   * Wants all missing numbers of row 9
+   */
+  @Test
+  public void getMissingNumbersOfLastRow() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 1, 9);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInRow(9));
+  }
+
+  /**
+   * Wants all missing numbers of row 1 (none in this case)
+   */
+  @Test
+  public void noNumberMissingInFirstRow() {
+    aSudoku.setValue(1, 1, 1);
+    aSudoku.setValue(2, 2, 1);
+    aSudoku.setValue(3, 3, 1);
+    aSudoku.setValue(4, 4, 1);
+    aSudoku.setValue(5, 5, 1);
+    aSudoku.setValue(6, 6, 1);
+    aSudoku.setValue(7, 7, 1);
+    aSudoku.setValue(8, 8, 1);
+    aSudoku.setValue(9, 9, 1);
+    assertArrayEquals(new int[]{}, aSudoku.getMissingNumbersInRow(1));
+  }
+
+  /**
+   * Wants all missing numbers of column 1
+   */
+  @Test
+  public void getMissingNumbersOfFirstColumn() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 1, 1);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInColumn(1));
+  }
+
+  /**
+   * Wants all missing numbers of column 5
+   */
+  @Test
+  public void getMissingNumbersOfMiddleColumn() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 5, 1);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInColumn(5));
+  }
+
+  /**
+   * Wants all missing numbers of column 9
+   */
+  @Test
+  public void getMissingNumbersOfLastColumn() {
+    int[] expectedArray = {2, 3, 4, 5, 6, 7, 8, 9};
+    aSudoku.setValue(1, 9, 1);
+    assertArrayEquals(expectedArray, aSudoku.getMissingNumbersInColumn(9));
   }
 }

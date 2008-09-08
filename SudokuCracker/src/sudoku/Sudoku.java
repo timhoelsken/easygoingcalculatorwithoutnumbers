066013a316168ea592be5da4a800a900092ce8b9
Tim.Hoelsken <Tim.Hoelsken@91bf1727-2546-0410-8e0a-abc221dbbbb9>
2008-09-08 15:08:04 +0000

    Initial import.

---
+++ SudokuCracker/src/sudoku/Sudoku.java
@@ -0,0 +1,45 @@
+package sudoku;
+
+/**
+ * 
+ * @author Tim
+ *
+ */
+public class Sudoku {
+
+  /**
+   * 
+   * @return Returns the dimension of the sudoku 
+   */
+  public int getDimension() {
+
+    return 9;
+  }
+
+  /**
+   * 
+   * Sets the given value on the field with the coordinates x and y
+   * 
+   * @param anX
+   * @param aY
+   * @param aValue
+   * @return Returns true if the content could be set 
+   */
+  public boolean setContent(int aValue, int anX, int aY) {
+    
+    
+    return true;
+  }
+
+  /**
+   * 
+   * @param anX
+   * @param aY
+   * @return Returns the value of the field with the coordinates x and y
+   */
+  public Object getField(int anX, int aY) {
+    
+    return 1;
+  }
+
+}
